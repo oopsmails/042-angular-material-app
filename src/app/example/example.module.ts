cff01eb2da207ec26c0e24c5598e391eb3cdd70a
oopsmails <oopsmails@gmail.com>
2022-08-04 10:23:04 -0400

    20220804: mess up

--- src/app/example/example.module.ts
+++ src/app/example/example.module.ts
@@ -1,21 +1,30 @@
+import { ModhyobittoTableComponent } from './components/modhyobitto-table/modhyobitto-table.component';
+import { ModhyobittoFormFieldComponent } from './components/modhyobitto-form-field/modhyobitto-form-field.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExampleHomeComponent } from './components/example-home/example-home.component';
 import { ExampleRoutingModule } from './example.routes';
 import { RouterModule, Routes } from '@angular/router';
+import { ModhyobittoDialogComponent } from './components/modhyobitto-dialog/modhyobitto-dialog.component';
 
 const routes: Routes = [
   { path: 'example', component: ExampleHomeComponent },
   { path: 'example/home', component: ExampleHomeComponent }
 ];
 
+
+const UTILITY_COMPONENTS = [
+  ExampleHomeComponent,
+  ModhyobittoFormFieldComponent,
+  ModhyobittoDialogComponent,
+  ModhyobittoTableComponent
+];
 @NgModule({
-  declarations: [
-    ExampleHomeComponent
-  ],
   imports: [
     CommonModule,
     ExampleRoutingModule
-  ]
+  ],
+  declarations: UTILITY_COMPONENTS,
+  exports: UTILITY_COMPONENTS
 })
 export class ExampleModule { }
--- src/app/sandbox/data-table/components/edit-table/edit-table.component.ts
+++ src/app/sandbox/data-table/components/edit-table/edit-table.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subject, Subscription } from 'rxjs';
 import { UserColumns, User } from '../../model/user';
+import { AppUtilityService } from '../../services/app-utility.service';
+import { FeatureModuleBService } from '../../services/feature-module-b.service';
 import { UserService } from '../../services/user.service';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
@@ -10,79 +14,125 @@ import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.compone
   templateUrl: './edit-table.component.html',
   styleUrls: ['./edit-table.component.scss']
 })
-export class EditTableComponent {
-  displayedColumns: string[] = UserColumns.map((col) => col.key);
-  columnsSchema = UserColumns;
-  dataSource = new MatTableDataSource<User>();
-  valid: any = {};
+export class EditTableComponent implements OnInit, OnDestroy {
 
-  constructor(public dialog: MatDialog, private userService: UserService) { }
-
-  ngOnInit() {
-    this.userService.getUsers().subscribe((res: any) => {
-      this.dataSource.data = res;
-    });
-  }
-
-  editRow(row: any) {
-    if (row.id === 0) {
-      this.userService.addUser(row).subscribe((newUser: User) => {
-        row.id = newUser.id;
-        row.isEdit = false;
-      });
-    } else {
-      this.userService.updateUser(row).subscribe(() => (row.isEdit = false));
+  table_config = {
+    columns:  [
+      {
+        key: 'a',
+        heading: 'Column A'
+      },
+      {
+        key: 'b',
+        heading: 'Column B'
+      },
+      {
+        key: 'c',
+        heading: 'Column C',
+        numeric: true
+      }
+    ],
+    primary_key_set: ['a'],
+    default_data: DUMMY_TABLE_DATA,
+    table_data_changer: new Subject<any>(),
+    ediTable: {
+      add: true,
+      edit: true
     }
-  }
+  };
 
-  addRow() {
-    const newRow: User = {
-      id: 0,
-      firstName: '',
-      lastName: '',
-      email: '',
-      birthDate: '',
-      isEdit: true,
-      isSelected: false,
-    };
-    this.dataSource.data = [newRow, ...this.dataSource.data];
-  }
+  // for table update form
+  is_table_being_updated: boolean = false;
+  is_new_row_being_added: boolean = false;
+  table_update_form!: FormGroup;
+  existing_row_values!: any;
 
-  removeRow(id: number) {
-    this.userService.deleteUser(id).subscribe(() => {
-      this.dataSource.data = this.dataSource.data.filter(
-        (u: User) => u.id !== id
-      );
-    });
-  }
-
-  removeSelectedRows() {
-    const users = this.dataSource.data.filter((u: User) => u.isSelected);
-    this.dialog
-      .open(ConfirmDialogComponent)
-      .afterClosed()
-      .subscribe((confirm) => {
-        if (confirm) {
-          this.userService.deleteUsers(users).subscribe(() => {
-            this.dataSource.data = this.dataSource.data.filter(
-              (u: User) => !u.isSelected
-            );
-          });
-        }
+  // for table update API call
+  private update_table_data_sub!: Subscription;
+  
+  constructor(
+    private global_utilities: AppUtilityService,
+    private feature_module_utilities: FeatureModuleBService
+    ) { 
+      this.table_update_form = new FormGroup({
+        a: new FormControl('',[Validators.required]),
+        b: new FormControl('',[]),
+        c: new FormControl('',[])
       });
   }
 
-  inputHandler(e: any, id: number, key: string) {
-    if (!this.valid[id]) {
-      this.valid[id] = {};
-    }
-    this.valid[id][key] = e.target.validity.valid;
+  ngOnInit(): void {
   }
 
-  disableSubmit(id: number) {
-    if (this.valid[id]) {
-      return Object.values(this.valid[id]).some((item) => item === false);
-    }
-    return false;
+  addNewRow(){
+    // enabling the primary key fields
+    this.global_utilities.toggleFormControls(this.table_update_form, this.table_config.primary_key_set, true);
+    // to reset the entire form
+    this.table_update_form.reset();
+    this.is_table_being_updated = true;
+    this.is_new_row_being_added =  true;
   }
+
+  editRow(row: any){
+    this.existing_row_values = {...row};
+    // to reset the entire form
+    this.table_update_form.reset();
+    // patch existing values in the form
+    this.table_update_form.patchValue(row);
+    // disabling the primary key fields
+    this.global_utilities.toggleFormControls(this.table_update_form, this.table_config.primary_key_set, false);
+    this.is_table_being_updated = true;
+    this.is_new_row_being_added =  false;
+  }
+
+  updateTableData(){
+    let updated_row_data = (this.is_new_row_being_added) ? {...this.table_update_form.value} : {...this.existing_row_values, ...this.table_update_form.value};
+
+    this.update_table_data_sub = this.feature_module_utilities.updateTableData(updated_row_data, this.is_new_row_being_added).subscribe(
+      (table_data: any)=>{
+        //close the drawer and reset the update form
+        this.is_table_being_updated = false;
+        this.table_update_form.reset();
+        //update the table with latest values
+        this.table_config.table_data_changer.next({
+          data: table_data,
+          highlight: updated_row_data
+        });
+      },
+      (error: any)=>{
+        this.global_utilities.showSnackbar();
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.global_utilities.unsubscribeAll([
+      this.update_table_data_sub
+    ]);
+  }
+
+
 }
+
+const DUMMY_TABLE_DATA: any[]= [
+  { a: 'Dummy1', b: 'Data String 1', c: 21},
+  { a: 'Dummy2', b: 'Data String 2', c: 22},
+  { a: 'Dummy3', b: 'Data String 3', c: 23},
+  { a: 'Dummy4', b: 'Data String 4', c: 24},
+  { a: 'Dummy5', b: 'Data String 5', c: 25},
+  { a: 'Dummy6', b: 'Data String 6', c: 26},
+  { a: 'Dummy7', b: 'Data String 7', c: 27},
+  { a: 'Dummy8', b: 'Data String 8', c: 28},
+  { a: 'Dummy9', b: 'Data String 9', c: 29},
+  { a: 'Dummy10', b: 'Data String 10', c: 30},
+  { a: 'Dummy11', b: 'Data String 11', c: 31},
+  { a: 'Dummy12', b: 'Data String 12', c: 32},
+  { a: 'Dummy13', b: 'Data String 13', c: 33},
+  { a: 'Dummy14', b: 'Data String 14', c: 34},
+  { a: 'Dummy15', b: 'Data String 15', c: 35},
+  { a: 'Dummy16', b: 'Data String 16', c: 36},
+  { a: 'Dummy17', b: 'Data String 17', c: 37},
+  { a: 'Dummy18', b: 'Data String 18', c: 38},
+  { a: 'Dummy19', b: 'Data String 19', c: 39},
+  { a: 'Dummy20', b: 'Data String 20', c: 40},
+];
